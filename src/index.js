46df8e738a4e072b41c2d038318c75e30029ff62
Sanesty-11 <sanesty11@gmail.com>
2023-09-14 11:25:49 -0400

    cart context and item count update implemented thru 3-25

--- src/index.js
+++ src/index.js
@@ -5,12 +5,15 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import ProductsContextProvider from "./context/products-context";
+import CartContextProvider from "./context/cart-context";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <BrowserRouter>
     <ProductsContextProvider>
-      <App />
+      <CartContextProvider>
+        <App />
+      </CartContextProvider>
     </ProductsContextProvider>
   </BrowserRouter>
 );

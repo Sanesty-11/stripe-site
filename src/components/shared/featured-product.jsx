9d8ab55e0550081a9b662e0453f4c1d3369f29c2
Sanesty-11 <sanesty11@gmail.com>
2023-09-14 18:23:03 -0400

    enabled product adding to cart from featured product page

--- src/components/shared/featured-product.jsx
+++ src/components/shared/featured-product.jsx
@@ -1,10 +1,13 @@
-import React from "react";
+import React, { useContext } from "react";
+import { isInCart } from "../../helpers";
+import { CartContext } from "../../context/cart-context";
 import { withRouter } from "react-router-dom";
 import "./featured-product.styles.scss";
 
 const FeaturedProduct = (props) => {
-  const { title, imageUrl, price, history, id } = props;
-
+  const { title, imageUrl, price, history, id, description } = props;
+  const product = { title, imageUrl, price, id, description };
+  const { addProduct, cartItems } = useContext(CartContext);
   return (
     <div className="featured-product">
       <div
@@ -16,7 +19,23 @@ const FeaturedProduct = (props) => {
       <div className="name-price">
         <h3>{title}</h3>
         <p>$ {price}</p>
-        <button className="button is-black roux-btn">Add to Cart</button>
+        {!isInCart(product, cartItems) && (
+          <button
+            className="button is-black roux-btn"
+            onClick={() => addProduct(product)}
+          >
+            Add to Cart
+          </button>
+        )}
+        {isInCart(product, cartItems) && (
+          <button
+            className="button is-white roux-btn"
+            id="btn-white-outline"
+            onClick={() => {}}
+          >
+            Add More
+          </button>
+        )}
       </div>
     </div>
   );
